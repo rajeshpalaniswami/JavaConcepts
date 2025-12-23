2ddb01c6d5fda05d000659c638e64fa08ca755ac
Rajesh.P. <rajeshpalaniswami@gmail.com>
2025-12-23 17:03:55 +0530

    Day13(23/12/2025)
    Day13(23/12/2025)

---
+++ src/com/daily/practice/collections/Dayo13.java
@@ -0,0 +1,24 @@
+package com.daily.practice.collections;
+
+public class Dayo13 {
+//Check if a String is Empty or Not
+	public static void main(String[] args) {
+		String givenStr = "";
+		String givenStr1 = " ";
+		
+		if (givenStr==null||givenStr.isEmpty()) {
+			System.out.println("The given String is empty string");
+		}else {
+			System.out.println("The given String is not an empty string");
+		}
+		
+		if (givenStr1==null||givenStr1.isEmpty()) {
+			System.out.println("The given String is empty string");
+		}else {
+			System.out.println("The given String is not an empty string");
+		}
+		
+		
+	}
+
+}
