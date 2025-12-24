5d169d05274e6f899bbef88c7106fc062e8022ed
Rajesh.P. <rajeshpalaniswami@gmail.com>
2025-12-24 17:30:15 +0530

    Day14(4/12/2025)
    Day14(4/12/2025)

---
+++ src/com/daily/practice/collections/Dayo14.java
@@ -0,0 +1,22 @@
+package com.daily.practice.collections;
+
+public class Dayo14 {
+	public static void main(String[] args) {
+		String givenStr = "Rajesh";
+		String givenStr1 = "Rajesh";
+			System.out.println( givenStr== givenStr1);
+			System.out.println( givenStr.equals(givenStr1));
+			
+			String a = "abcgh";
+			String b = "def";
+			String c= "";
+			
+			for (int i=0;i<b.length();i++) {
+				
+					c = c+ a.charAt(i)+b.charAt(i);
+				
+			}
+			System.out.println(c);
+	}
+
+}
