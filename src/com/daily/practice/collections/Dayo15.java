191f217e86a303d9156c1579575270d1cc15f743
Rajesh.P. <rajeshpalaniswami@gmail.com>
2025-12-26 17:10:15 +0530

    Day16 (26/12/2022)
    Day16 (26/12/2022)

--- src/com/daily/practice/collections/Dayo15.java
+++ src/com/daily/practice/collections/Dayo15.java
@@ -1,27 +1,28 @@
 package com.daily.practice.collections;
 
 public class Dayo15 {
-// If a string is longer than the other, append the additional letters onto the end of the merged string.
+// If a string is longer than the other, 
+//append the additional letters onto the end of the merged string.
 	public static void main(String[] args) {
-		String a ="abcd";
+		String a = "abcd";
 		String b = "efghij";
-		
-		String c ="";
-		
+
+		String c = "";
+
 		int minLength = Math.min(a.length(), b.length());
-		
-		for (int i=0;i<minLength;i++) {
-			c = c+a.charAt(i)+b.charAt(i);
+
+		for (int i = 0; i < minLength; i++) {
+			c = c + a.charAt(i) + b.charAt(i);
 		}
-		
+
 		System.out.println(c);
-		
-		if (minLength>a.length()) {
-			c = c+ a.substring(minLength);
-		}else {
-			c +=  b.substring(minLength);	
+
+		if (minLength > a.length()) {
+			c = c + a.substring(minLength);
+		} else {
+			c += b.substring(minLength);
 		}
-		
+
 		System.out.println(c);
 
 	}
---
+++ src/com/daily/practice/collections/Dayo16.java
@@ -0,0 +1,13 @@
+package com.daily.practice.collections;
+
+public class Dayo16 {
+
+	public static void main(String[] args) {
+	
+	int randomNum = (int) (Math.random()*101);
+	System.out.println(randomNum);
+	
+
+	}
+
+}
