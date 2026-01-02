a720fc91f84efa616b05b23a3a8fdf00adb36545
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-01-02 17:38:51 +0530

    Day21(2/2/2026)
    Day21(2/2/2026)

---
+++ src/com/chapter/two/IfClass1.java
@@ -0,0 +1,16 @@
+package com.chapter.two;
+
+public class IfClass1 {
+//Find the smallest of two numbers.
+	public static void main(String[] args) {
+		
+int a = 700 ;
+int b = 200 ;
+if (a<b) {
+	System.out.println("a is the smalest number");
+}else {
+	System.out.println("b is the smalest number");
+}
+	}
+
+}
