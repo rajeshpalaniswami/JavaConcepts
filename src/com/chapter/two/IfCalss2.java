96a0a5ff524b9e77ba01a5384db4907e8521ef86
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-01-03 21:44:48 +0530

    Day22(3/1/2026)
    Day22(3/1/2026)

---
+++ src/com/chapter/two/IfCalss2.java
@@ -0,0 +1,21 @@
+package com.chapter.two;
+
+import java.util.Scanner;
+
+public class IfCalss2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+	 System.out.println("Enter the number");
+	 int number = scan.nextInt();
+	 
+	 if(number%5==0) {
+		 System.out.println("Yes the given number is multiple of 5 and the number is "+number);
+		 
+	 }else {
+		 System.out.println("No the given number is not a multiple of 5 and the number is "+number);
+	 }
+
+	}
+
+}
