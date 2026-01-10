ac4519f1a3ec163ae375c142dcfdb17fa9638c73
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-01-10 21:28:22 +0530

    Day29 (10/1/2026)
    Day29 (10/1/2026)

---
+++ src/com/chapter/two/IfClass9.java
@@ -0,0 +1,35 @@
+package com.chapter.two;
+
+import java.util.Scanner;
+
+public class IfClass9 {
+
+	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please enter the length - first side of the triangle");
+		int a =scan.nextInt();
+		System.out.println("Please enter the length - second side of the triangle");
+		int b= scan.nextInt();
+		System.out.println("Please enter the length - third side of the triangle");
+		int c=scan.nextInt();
+		
+		
+		if (a+b>c&&b+c>a&&c+a>b) {
+			System.out.println("yes the given lenghts are valid for a triangle");
+			
+			if (a==b&b==c&c==a) {
+				System.out.println("Based on the given sides it is a equilateral tringle");
+			}else if (a==b||b==c||c==a) {
+				System.out.println("Based on the given sides it is a isosceles tringle");
+			}else {
+				System.out.println("Based on the given sides it is a scalene tringle");
+			}
+			
+		}else {
+			System.out.println("No the given lenghts are not a valid for triangle");
+		}
+		scan.close();		
+	}
+
+}
--- src/com/chapter/two/Ifclass8.java
+++ src/com/chapter/two/Ifclass8.java
@@ -5,23 +5,28 @@ import java.util.Scanner;
 public class Ifclass8 {
 
 	public static void main(String[] args) {
-		//Find profit or loss based on cost price and selling price.
-		Scanner scan = new Scanner (System.in);
-		System.out.println("Enter the value of cost prise");
-		int costPrise = scan.nextInt();
-		System.out.println("Enter the value of selling prise");
-		int sellingPrise = scan.nextInt();
-		System.out.println("Enter your option profit/loss");
-		String option = scan.next();
-		
-		if (costPrise <sellingPrise ) {
-			System.out.println("yes you are in profit");
-		}else {
-			System.out.println("Sorry your are in loss");
-		}
-		
 
-	}
+        // Find profit or loss based on cost price and selling price.
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Enter the value of Cost Price:");
+        int costPrice = scan.nextInt();
+
+        System.out.println("Enter the value of Selling Price:");
+        int sellingPrice = scan.nextInt();
+
+        if (sellingPrice > costPrice) {
+            System.out.println("You are in Profit");
+        } 
+        else if (sellingPrice < costPrice) {
+            System.out.println("You are in Loss");
+        } 
+        else {
+            System.out.println("No Profit and No Loss");
+        }
+
+        scan.close();
+        
+    }
 }
 
