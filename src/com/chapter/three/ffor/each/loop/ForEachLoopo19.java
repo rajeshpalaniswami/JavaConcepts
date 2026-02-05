8759fb549ca15187b3a19a611fd24fb5e78dd1b5
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-02-05 15:10:23 +0530

    Day54 (5/2/2025)
    Day54 (5/2/2025)

---
+++ src/com/chapter/three/ffor/each/loop/ForEachLoopo19.java
@@ -0,0 +1,23 @@
+package com.chapter.three.ffor.each.loop;
+
+public class ForEachLoopo19 {
+
+	public static void main(String[] args) {
+		// Find common elements between two arrays.
+		
+		int [] array1 = {1,2,3,4,5};
+		int [] array2 = {2,5,0,6,7};
+		
+		for (int a : array1) {
+			for (int b:array2) {
+				if (!(a==b)) {
+					System.out.println(a);
+					break;
+				}
+			}
+		}
+		
+
+	}
+
+}
