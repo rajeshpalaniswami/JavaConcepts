df6bb4a883b2765ec7f0628a6aada817fe5eeb1c
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-02-11 15:12:46 +0530

    Day61 (11/2/2026)
    Day61 (11/2/2026)

---
+++ src/com/chapter/four/switchstatements/SwitchOne.java
@@ -0,0 +1,20 @@
+package com.chapter.four.switchstatements;
+
+public class SwitchOne {
+
+	public static void main(String[] args) {
+		int a = 110;
+		switch (a) {
+		case 10 : System.out.println("1");
+		break;
+		case 20 : System.out.println("2");
+		break;
+		case 30 : System.out.println("3");
+		break;
+		default : System.out.println("default");
+		break;
+		}
+
+	}
+
+}
---
+++ src/com/chapter/four/switchstatements/SwitchTwo.java
@@ -0,0 +1,34 @@
+package com.chapter.four.switchstatements;
+
+import java.util.Scanner;
+
+public class SwitchTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the number to get the day");
+
+		int a = scan.nextInt();
+
+		while (a < 1 || a > 7) {
+		    System.out.println("Please enter a valid number between 1 and 7:");
+		    a = scan.nextInt();
+		}
+
+		switch (a) {
+		case 1: System.out.println("Monday"); break;
+		case 2: System.out.println("Tuesday"); break;
+		case 3: System.out.println("Wednesday"); break;
+		case 4: System.out.println("Thursday"); break;
+		case 5: System.out.println("Friday"); break;
+		case 6: System.out.println("Saturday"); break;
+		case 7: System.out.println("Sunday"); break;
+		}
+
+		scan.close();
+		}
+
+	}
+
+
