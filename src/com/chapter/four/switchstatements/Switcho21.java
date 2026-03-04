695e6eaa12d606fcb78f2a59f017a5e71bf47c18
Rajesh.P. <rajeshpalaniswami@gmail.com>
2026-03-04 14:31:07 +0530

    Day73(4/3/2026)
    Day73(4/3/2026)

--- src/com/chapter/four/switchstatements/Switcho21.java
+++ src/com/chapter/four/switchstatements/Switcho21.java
@@ -1,10 +1,13 @@
 package com.chapter.four.switchstatements;
 
-public class Switcho18 {
+public class Switcho21 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		for (int i=0;i<=10;i++) {
+			System.out.println("2*"+i+"="+2*i);
+		}
+
 	}
 
 }
